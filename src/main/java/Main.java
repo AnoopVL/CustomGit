597b50d45c54255c4eebc80e8c995b8de1a05107
AnoopVL <avlanjekar4@gmail.com>
2024-09-10 21:16:47 +0530

    Read blob objects

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,15 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
+import java.util.zip.InflaterInputStream;
 
 public class Main {
   public static void main(String[] args){
     
     final String command = args[0];
-    // demo commit 
     switch (command) {
       case "init" -> {
         final File root = new File(".git");
@@ -19,6 +22,27 @@ public class Main {
           Files.write(head.toPath(), "ref: refs/heads/main\n".getBytes());
           System.out.println("Initialized git directory");
         } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+
+      case "cat-file" ->{
+        if(!"-p".equals(args[1])){
+          System.out.println("Incorrect arguments for cat-file");
+          return;
+        }
+        String hash = args[2];
+        String dir = hash.substring(0,2);
+        hash = hash.substring(2);
+        
+        File file = new File("./.git/objects/" + dir + "/" + hash);
+        try {
+          String blobObj = new BufferedReader(new InputStreamReader(new InflaterInputStream(new FileInputStream(file)))).readLine();
+          
+          String content = blobObj.substring(blobObj.indexOf('\0') + 1);
+          System.out.print(content);
+          
+        }catch(IOException e){
           throw new RuntimeException(e);
         }
       }
